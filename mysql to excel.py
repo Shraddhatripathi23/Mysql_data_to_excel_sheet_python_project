9d036c3a7106be39e3559c64792de7292e2b439c
Shraddha Tripathi <50836422+Shraddhatripathi23@users.noreply.github.com>
2023-09-13 01:28:06 +0530

    Add files via upload

---
+++ mysql to excel.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install mysql-connector-python
+
+
+# In[2]:
+
+
+import mysql.connector as connection
+import pandas as pd
+
+
+# In[3]:
+
+
+sql = "SELECT * FROM student ;"
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+#db = connection.connect(host="localhost", database="my_db", user="root", password="Root@123",  use_pure=True) 
+db = connection.connect(host='127.0.0.1', user="root",   password ="Root@123", database="my_db" , auth_plugin='mysql_native_password')
+
+
+# In[5]:
+
+
+df = pd.read_sql(sql, db)
+
+
+# In[6]:
+
+
+df.head()
+
+
+# In[7]:
+
+
+df.to_excel("mysql_date.xlsx", index=False)
+
+
+# In[ ]:
+
+
+
+
